2a3f5b5ff120ce67dc395229082f9e6272cc178f
andersbaath <anders.baath@faceitab.se>
2016-11-17 11:16:18 +0100

    V 0.11

--- sh2web-functions.php
+++ sh2web-functions.php
@@ -1,10 +1,9 @@
 <?php
-## Version 0.1
+## Version 0.11
 ### FunctionShDupLightSetScenario ###########################################################
 function FunctionShDupLightSetScenario($CONTROLLER, $USERNAME, $PASSWORD, $SCENARIO, $ID) {
 	$LOGONURL = "http://$CONTROLLER/index.php";
 	$url = $LOGONURL;
-
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 	curl_setopt($ch, CURLOPT_URL, $url); 
@@ -17,9 +16,7 @@ function FunctionShDupLightSetScenario($CONTROLLER, $USERNAME, $PASSWORD, $SCENA
 	curl_setopt($ch, CURLOPT_COOKIEFILE,      $cookie);
 	curl_setopt ($ch, CURLOPT_POST, 1); 
 	curl_setopt ($ch,CURLOPT_POSTFIELDS,"username=$USERNAME&password=$PASSWORD");     
-
 	$resultA = curl_exec($ch);
-
 	$curl_header = array('X-Requested-With: XMLHttpRequest');
 	$data = "param=$ID&st=-1&action=s$SCENARIO&mvm=-1";
 	$COMMANDURL = "http://$CONTROLLER/do.php?command=rollerSendCmd";
@@ -31,18 +28,16 @@ function FunctionShDupLightSetScenario($CONTROLLER, $USERNAME, $PASSWORD, $SCENA
 	curl_setopt($ch, CURLOPT_VERBOSE, false);
 	curl_setopt($ch, CURLOPT_COOKIEJAR,       $cookie);
 	curl_setopt($ch, CURLOPT_COOKIEFILE,      $cookie);
-
 	$resultB = curl_exec($ch);
 	curl_close($ch);
 	return $SCENARIO;
 	}
-##############################################################################################
+#############################################################################################
 
 ### FunctionShDupLightToggleState ###########################################################
 function FunctionShDupLightToggleState($CONTROLLER, $USERNAME, $PASSWORD, $ID) {
         $LOGONURL = "http://$CONTROLLER/index.php";
         $url = $LOGONURL;
-
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
         curl_setopt($ch, CURLOPT_URL, $url); 
@@ -55,9 +50,7 @@ function FunctionShDupLightToggleState($CONTROLLER, $USERNAME, $PASSWORD, $ID) {
         curl_setopt($ch, CURLOPT_COOKIEFILE,      $cookie);
         curl_setopt ($ch, CURLOPT_POST, 1); 
         curl_setopt ($ch,CURLOPT_POSTFIELDS,"username=$USERNAME&password=$PASSWORD");     
-
         $result = curl_exec($ch);
-
         $curl_header = array('X-Requested-With: XMLHttpRequest');
         $data = "param=$ID&st=-1";
         $COMMANDURL = "http://$CONTROLLER/do.php?command=do";
@@ -69,18 +62,16 @@ function FunctionShDupLightToggleState($CONTROLLER, $USERNAME, $PASSWORD, $ID) {
         curl_setopt($ch, CURLOPT_VERBOSE, false);
         curl_setopt($ch, CURLOPT_COOKIEJAR,       $cookie);
         curl_setopt($ch, CURLOPT_COOKIEFILE,      $cookie);
-
         $result = curl_exec($ch);
         curl_close($ch);
 }
 ##############################################################################################
 
-### FunctionShDupLightStatus ###################################################################
+### FunctionShDupLightStatus #################################################################
 ### If $SWITCH is set to 0 all functions values are returned if $SWITCH is set to 1 only state is returned 1/0 (on/off)
 function FunctionShDupLightStatus($CONTROLLER, $USERNAME, $PASSWORD, $ID, $SWITCH) {
         $LOGONURL = "http://$CONTROLLER/index.php";
         $url = $LOGONURL;
-
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -93,18 +84,13 @@ function FunctionShDupLightStatus($CONTROLLER, $USERNAME, $PASSWORD, $ID, $SWITC
         curl_setopt($ch, CURLOPT_COOKIEFILE,      $cookie);
         curl_setopt ($ch, CURLOPT_POST, 1);
         curl_setopt ($ch,CURLOPT_POSTFIELDS,"username=$USERNAME&password=$PASSWORD");
-
         $result = curl_exec($ch);
-
 	$LOGONURL = "http://$CONTROLLER/refresh.php?param[0][]=switch$ID&param[0][]=$ID&param[0][]=switch&param[1][]=function_state&param[1][]=$ID&param[1][]=text";
         $url = $LOGONURL;
-
         curl_setopt ($ch, CURLOPT_POST, 0);
         curl_setopt($ch, CURLOPT_URL, $url);
         $result = curl_exec($ch);
-
         $RESULTARRAY = json_decode($result, true);
-
         if ($SWITCH == 1){
 		return $RESULTARRAY["switch$ID"][1];
         }
@@ -120,7 +106,6 @@ function FunctionShDupLightStatus($CONTROLLER, $USERNAME, $PASSWORD, $ID, $SWITC
 function FunctionShDupTempRegStatus($CONTROLLER, $USERNAME, $PASSWORD, $ID, $ROOMTEMP) {
         $LOGONURL = "http://$CONTROLLER/index.php";
         $url = $LOGONURL;
-
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
         curl_setopt($ch, CURLOPT_URL, $url); 
@@ -133,18 +118,13 @@ function FunctionShDupTempRegStatus($CONTROLLER, $USERNAME, $PASSWORD, $ID, $ROO
         curl_setopt($ch, CURLOPT_COOKIEFILE,      $cookie);
         curl_setopt ($ch, CURLOPT_POST, 1); 
         curl_setopt ($ch,CURLOPT_POSTFIELDS,"username=$USERNAME&password=$PASSWORD");     
-
         $result = curl_exec($ch);
-
         $LOGONURL = "http://$CONTROLLER/refresh.php?param[0][]=active_set&param[0][]=$ID&param[0][]=text&param[1][]=heating_status&param[1][]=$ID&param[1][]=text&param[2][]=troom_value&param[2][]=$ID&param[2][]=text";
         $url = $LOGONURL;
-
 	curl_setopt ($ch, CURLOPT_POST, 0); 
 	curl_setopt($ch, CURLOPT_URL, $url);
 	$result = curl_exec($ch);
-
 	$RESULTARRAY = json_decode($result, true);
-
 	if ($ROOMTEMP == 1){
 		return substr($RESULTARRAY["troom_value"][1]["value"], 0, -3);
 	}
